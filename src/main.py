780e49d26809d7042bb60221d1db7ffa29c54830
Eduardo Angulo <edangulo@uninorte.edu.co>
2023-09-19 19:51:55 -0500

    Created main file and project modules

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from person import Autor
+
+def main() -> None:
+    nombre = input('Digite el nombre: ')
+    cedula = int(input('Digite la cedula: '))
+
+    autor = Autor(nombre, cedula)
+
+    print(autor)
+
+
+if __name__ == '__main__':
+    main()
